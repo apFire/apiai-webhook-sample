aff9cc0a80b3930f55979f09e51bc09ee18474c2
Abhishek Pande <abhishek.pande24@gmail.com>
2017-05-17 22:59:08 +0530

    Resolved issue I think

--- index.js
+++ index.js
@@ -26,50 +26,7 @@ restService.post('/webhook', function (req, res) {
                 }
 
                 if (requestBody.result.action) {
-                    speech += 'action: ' + requestBody.result.action;g'	
-                }
-            }
-        }
-
-        console.log('result: ', speech);
-
-        return res.json({
-            speech: speech,
-            displayText: speech,
-            source: 'apiai-webhook-sample'
-        });
-    } catch (err) {
-        console.error("Can't process request", err);
-
-        return res.status(400).json({
-            status: {
-                code: 400,
-                errorType: err.message
-            }
-        });
-    }
-});
-
-restService.post('/hook', function (req, res) {
-
-    console.log('hook request');
-
-    try {
-        var speech = 'empty speech';
-
-        if (req.body) {
-            var requestBody = req.body;
-
-            if (requestBody.result) {
-                speech = '';
-
-                if (requestBody.result.fulfillment) {
-                    speech += requestBody.result.fulfillment.speech;
-                    speech += ' awesome hook';	
-                }
-
-                if (requestBody.result.action) {
-                    speech += 'action: ' + requestBody.result.action;g'	
+                    speech += 'action: ' + requestBody.result.action;	
                 }
             }
         }
