bd9d5d74ef2fc80dda9396aeea91b52cf35e9bee
Abhishek Pande <abhishek.pande24@gmail.com>
2017-05-17 22:27:13 +0530

    updated webhook to test integration

--- index.js
+++ index.js
@@ -22,11 +22,13 @@ restService.post('/hook', function (req, res) {
 
                 if (requestBody.result.fulfillment) {
                     speech += requestBody.result.fulfillment.speech;
-                    speech += ' ';
+                    speech += ' awesome hook';
+		    displayText = 'Hooked on an feeling'	
                 }
 
                 if (requestBody.result.action) {
                     speech += 'action: ' + requestBody.result.action;
+		    displayText = 'Hooked on an feeling'	
                 }
             }
         }
