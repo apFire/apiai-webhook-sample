288ffc71b630290456a29cf8b9a955e701023dbe
Abhishek Pande <abhishek.pande24@gmail.com>
2017-05-19 11:01:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,32 +38,24 @@ restService.post('/webhook', function (req, res) {
                         messages.push({ "type": 0,"platform":"skype","speech": speech});
                         messages.push({ "type": 0,"platform":"skype","speech": 'QB111111 Intraday description'});
                         messages.push({ "type": 0,"platform":"skype","speech": 'QB111112 Intraday description'});
-                        messages.push({ "type": 0,"platform":"default","speech": speech});
-                        messages.push({ "type": 0,"platform":"default","speech": 'QB111111 Intraday description'});
-                        messages.push({ "type": 0,"platform":"default","speech": 'QB111112 Intraday description'});
+                        messages.push({ "type": 0,"platform":"default","speech": speech + 'QB111111 Intraday description' + 'QB111112 Intraday description'});
                     } else if(requestBody.result.parameters.i1ProjectArtifact === 'documentation') { 
                         speech += 'Here are ther Quickbase projects for ' + requestBody.result.parameters.i1project;
                         messages.push({ "type": 0, "platform": "skype","speech": speech});
                         messages.push({ "type": 0, "platform": "skype","speech": 'QB111111 Intraday description'});
                         messages.push({ "type": 0, "platform": "skype","speech": 'QB111112 Intraday description'});
-                        //messages.push({ "type": 0,"speech": speech});
-                        //messages.push({ "type": 0,"speech": 'QB111111 Intraday description'});
-                        //messages.push({ "type": 0,"speech": 'QB111112 Intraday description'}); 
+                        messages.push({ "type": 0,"platform":"default","speech": speech + 'QB111111 Intraday description' + 'QB111112 Intraday description'});
                     } else if(requestBody.result.parameters.i1ProjectArtifact === 'SME') { 
                         speech += 'SME for ' + requestBody.result.parameters.i1project + ' projects are';
-                        messages.push({ "type": 0, "platform": "skype","speech": speech});
-                        messages.push({ "type": 0, "platform": "skype","speech": 'Jayant (9999)'});
-                        messages.push({ "type": 0, "platform": "skype","speech": 'Gaurav (9999)'});
-                        messages.push({ "type": 0, "platform": "skype","speech": 'Niranjan (9999)'});
-                        messages.push({ "type": 0, "platform": "skype","speech": 'These guys will help you'});
-                        //messages.push({ "type": 0,"speech": speech});
-                        //messages.push({ "type": 0,"speech": 'Jayant (9999)'});
-                        //messages.push({ "type": 0,"speech": 'Gaurav (9999)'});
-                        //messages.push({ "type": 0,"speech": 'Niranjan (9999)'});
-                        //messages.push({ "type": 0,"speech": 'These guys will help you'});
+                        messages.push({ "type": 0,"platform": "skype","speech": speech});
+                        messages.push({ "type": 0,"platform": "skype","speech": 'Jayant (9999)'});
+                        messages.push({ "type": 0,"platform": "skype","speech": 'Gaurav (9999)'});
+                        messages.push({ "type": 0,"platform": "skype","speech": 'Niranjan (9999)'});
+                        messages.push({ "type": 0,"platform": "skype","speech": 'These guys will help you'});
+                        messages.push({ "type": 0,"platform":"default","speech": speech + 'Jayant (9999) Gaurav (9999) Niranjan (9999). These guys will help you.'});
                     } else { 
-                       messages.push({ "type": 0, "platform": "skype","speech": 'Sorry I can not help you with this but I guess Jayant can. you should get in touch'});   
-                       //messages.push({ "type": 0,"speech": 'Sorry I can not help you with this but I guess Jayant can. you should get in touch'}); 
+                       messages.push({ "type": 0,"platform":"skype","speech": 'Sorry I can not help you with this but I guess Jayant can. you should get in touch'});   
+                       messages.push({ "type": 0,"platform":"default""speech": 'Sorry I can not help you with this but I guess Jayant can. you should get in touch'}); 
                     }
                     }
                 }
